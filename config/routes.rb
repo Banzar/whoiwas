2b20050b94df19a9c61a0ddb73e7e065d0e158c7
David C. Ledford <dcledford@ymail.com>
2018-03-09 13:57:20 -0500

    users controllers started

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/index'
+
+  get 'signup', to: 'users#new'
+
+  get 'users/edit'
+
+  get 'users/show'
+
+  root 'home#index'
+  
+  get 'home/index'
+
+  resources :users
 end
