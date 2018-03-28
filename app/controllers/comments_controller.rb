c1d7f8613668ec0ea2c6caa55e3c01a88725fcd1
David C. Ledford <dcledford@ymail.com>
2018-03-27 23:43:38 -0400

    Comment create/edit/update/destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
 	before_action :find_legacy
+	before_action :find_comment, only: [:destroy, :edit, :update]
 	def create
 		@comment = @legacy.comments.create(params[:comment].permit(:content))
 		@comment.user_id = current_user.id
@@ -12,10 +13,35 @@ class CommentsController < ApplicationController
 		end
 	end
 
+	def destroy
+		@comment.destroy
+		redirect_to legacy_path(@legacy)
+	end
+
+	def edit
+		
+	end
+
+	def update
+		if @comment.update(comment_params)
+			redirect_to legacy_path(@legacy)
+
+		else
+			render 'edit'
+		end	
+	end
+
 
 	private
+	def comment_params
+		params[:comment].permit(:content)	
+	end
 
 	def find_legacy
 		@legacy = Legacy.find(params[:legacy_id])
+	end
+
+	def find_comment
+		@comment = @legacy.comments.find(params[:id])
 	end
 end
