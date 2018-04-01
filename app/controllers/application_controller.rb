968c1146166d8fd4431b1555fa1f8c6c63f276a4
David C. Ledford <dcledford@ymail.com>
2018-03-31 21:34:38 -0400

    starting to work on mobile site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  before_action :detect_device_variant
+
+  private
+
+  def detect_device_variant
+    request.variant = :tablet if browser.device.tablet?
+    request.variant = :desktop if !browser.device.mobile? && !browser.device.tablet?
+  end
 end
