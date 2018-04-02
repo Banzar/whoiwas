3d5a67288a77a012f369d143350db51289b8407a
David C. Ledford <dcledford@ymail.com>
2018-04-01 22:29:51 -0400

    went back to the way it was before I tried using mobile.css for mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
-
-  def mobile_device?
-  	request.user_agent =~ /Mobile|webOS/
-  end
-  helper_method :mobile_device?
 end
