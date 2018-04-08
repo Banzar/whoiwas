832d950ab678357b43762cb537c7df094f1e084c
David C. Ledford <dcledford@ymail.com>
2018-04-07 23:37:13 -0400

    adding forgot password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -6,16 +6,15 @@ class PasswordResetsController < ApplicationController
   end
 
   def create
-  	# @user = User.find_by(:email params[:password_reset][:email].downcase)
-  	# if @user
-  	# 	@user.create_reset_digest
-  	# 	@user.send_password_reset_email
-  	# 	flash[:notice] = "Email sent with password reset instructions"
-  	# 	redirect_to root_url
-
-  	# else
-  	# 	flash[:notice] = "Email address was not found."
-  	# 	render 'new'
-   #  end
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "Email sent with password reset instructions"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "Email address not found"
+      render 'new'
+    end
   end
 end
