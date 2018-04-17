e36c9f6273fd220d0f94cf166fa2450d22642dab
David C. Ledford <dcledford@ymail.com>
2018-04-17 15:49:24 -0400

    testing with every 1.minute for cron jobs, added <h1>Testing currently</h1> to bottom to make sure all is pushing

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 
-every 1.day, at: '3:40 pm' do
+every 1.minute do
   runner "unactivated = User.where('activated = ?', false)"
   runner "unactivated_expired = unactivated.where('date(created_at) <= ?', 3.days.ago)"
   runner "unactivated_expired.find_each(&:destroy)"
